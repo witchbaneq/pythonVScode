c75449c3e2da31765845057820a0c3f789b23b89
Emotional-emptiness <moon.linexx@gmail.com>
2025-06-13 21:05:03 +0300

    Refactor input handling and error management in try_exept.py

--- try_exept.py
+++ try_exept.py
@@ -8,3 +8,17 @@ while not is_ok:
         print("Вы ввели не число, попробуйте еще раз")
 
 print(number)
+
+
+def divide(a, b): # Функция для деления двух чисел
+    result = a / b
+    return result
+
+good = False
+while not good: # Цикл для повторного запроса ввода в случае ошибки
+    try:
+        result = divide(int(input("Введите делимое: ")), int(input("Введите делитель: ")))
+        print(result)
+        good = True
+    except (ZeroDivisionError, ValueError):
+        print("Ошибка ввода, попробуйте еще раз")
