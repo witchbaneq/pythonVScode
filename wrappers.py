9cf6266e10e399c57fe04035e4bf6a6e5d828b86
Emotional-emptiness <moon.linexx@gmail.com>
2025-06-15 21:45:27 +0300

    Refactor logger decorator and apply it to say_bye function

--- wrappers.py
+++ wrappers.py
@@ -1,4 +1,17 @@
+def logger(func):
+    def wrapper():  
+        print("function started")
+        func()
+        print("function ended")
+    return wrapper
+
+@logger
 def say_hi():
     print("hi")
 
+@logger
+def say_bye():
+    print("bye")
+
 say_hi()
+say_bye()
