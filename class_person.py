46078ebeb78ab4ec849e47ffe5cfc4e0d97ac0f6
Emotional-emptiness <moon.linexx@gmail.com>
2025-06-22 21:00:06 +0300

    Add car_color attribute to Person class and update instances in PrintPerson

--- class_person.py
+++ class_person.py
@@ -1,6 +1,10 @@
 class Person: 
-    def __init__(self, name, age, country, gender): # Constructor
+    def __init__(self, name, age, country, gender, car_color = None): # Constructor
         self.country = country
         self.name = name
         self.age = age
         self.gender = gender
+        self.car_color = car_color
+
+    def say_name(self, last_name):
+        print(f"Hello {self.name} {last_name}.")
--- print_person.py
+++ print_person.py
@@ -2,7 +2,7 @@ from class_person import Person
 # Это созадет экземпляр класса Person и вызывает метод print_person_info для каждого экземпляра.
 
 class PrintPerson:
-    person_one = Person("John",30, "USA", "male")
+    person_one = Person("John",30, "USA", "male", "red")
     person_two = Person("Alice", 25, "UK", "female")
 
     def print_person_info(person):
@@ -11,3 +11,5 @@ class PrintPerson:
 
     print_person_info(person_one)
     print_person_info(person_two)
+
+    person_one.say_name("Smith")
